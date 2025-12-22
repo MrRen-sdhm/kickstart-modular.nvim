a16dd9a2d661bcd9ff1e3b1b03aeb6c2b2582594
sdhm <rzy.1996@qq.com>
2025-12-23 00:25:20 +0800

    todo-comments add keywords for DEBUG

--- lua/kickstart/plugins/todo-comments.lua
+++ lua/kickstart/plugins/todo-comments.lua
@@ -1,5 +1,25 @@
 -- Highlight todo, notes, etc in comments
 return {
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = {
+      signs = false,
+      keywords = {
+        DEBUG = { icon = "", color = "error", alt = { "DBG", "DEBG" } }, -- add key words for debug
+      },
+    }
+  },
 }
+
+-- TODO:
+-- FIXME:
+-- NOTE:
+-- HACK:
+-- WARN:
+-- PERF:
+-- TEST:
+-- DEBUG:
+
 -- vim: ts=2 sts=2 sw=2 et
