237a5703a8d63280b34fb30cd623a51487f7b2b0
sdhm <rzy.1996@qq.com>
2026-01-03 23:53:26 +0800

    disable sync clipboard

--- lua/options.lua
+++ lua/options.lua
@@ -22,9 +22,11 @@ vim.o.showmode = false
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.schedule(function()
-  vim.o.clipboard = 'unnamedplus'
-end)
+-- NOTE: disable sync clipboard
+-- vim.schedule(function()
+--   vim.o.clipboard = 'unnamedplus'
+-- end)
+vim.o.clipboard = ''
 
 -- Enable break indent
 vim.o.breakindent = true
