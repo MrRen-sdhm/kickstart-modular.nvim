89a140739f076f91a4f4dd17da5a8e4e8d3d5024
sdhm <rzy.1996@qq.com>
2026-01-25 20:30:24 +0800

    flash.nvim enhance

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -3,6 +3,11 @@ return {
   event = "VeryLazy",
   ---@type Flash.Config
   opts = {
+    labels = "asdfjklghcvnmeruiwotybzxqp",
+    label = {
+      -- allow uppercase labels
+      uppercase = true,
+    },
     modes = {
         -- disable enhanced [f F t T ; ,]
         char = { enabled = false },
@@ -16,9 +21,18 @@ return {
   },
   keys = {
     { "f", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "F", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "F", mode = { "n", "x", "o" }, function()
+      require("flash").treesitter({
+        actions = {["<CR>"] = "next", ["<BS>"] = "prev"}
+      }) end, desc = "Flash Treesitter" },
     { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
     { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
     { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    { "<leader>j", mode = { "n", "x", "o" }, function()
+      require("flash").jump({
+        search = { mode = "search", max_length = 0 },
+        label = { after = { 0, 0 } },
+        pattern = "^"
+      }) end, desc = "Flash jump to a line" },
   },
 }
