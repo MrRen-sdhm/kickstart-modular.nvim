8ebd8f77c373483eca52ea7d17c929acedb78229
sdhm <rzy.1996@qq.com>
2026-03-08 20:58:00 +0800

    add two markdown preview plugins

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -44,6 +44,17 @@ require('lazy').setup({
       stages = "fade_in_slide_out", -- fade_in_slide_out fade slide static
     },
   },
+  {
+    "OXY2DEV/markview.nvim",
+    lazy = false,
+    enabled = true,
+  },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    opts = {},
+    enabled = false,
+  },
 
   -- colorscheme
   {
