2c9c473b2af7dc80bc7fe74da88ed7c6de645441
sdhm <rzy.1996@qq.com>
2026-03-22 16:43:08 +0800

    treesitter do not auto install & tmux navigator disable when zoom

--- lua/plugins/vim-tmux-navigator.lua
+++ lua/plugins/vim-tmux-navigator.lua
@@ -2,6 +2,7 @@ return {
   "christoomey/vim-tmux-navigator",
   lazy = false,
   init = function()
+    vim.g.tmux_navigator_disable_when_zoomed = 1
     vim.g.tmux_navigator_no_mappings = 1
     vim.keymap.set("n", "<M-Left>", "<cmd>TmuxNavigateLeft<CR>")
     vim.keymap.set("n", "<M-Down>", "<cmd>TmuxNavigateDown<CR>")
